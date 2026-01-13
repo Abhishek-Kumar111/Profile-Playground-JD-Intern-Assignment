6606d91917b5f34bb05a2a72f218be93b8b1c754
Abhi <b23cs025@nitm.ac.in>
2026-01-13 23:37:12 +0530

    modified one sentense

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ const publicPath = path.join(__dirname, "public");
 app.use(express.static(publicPath));
 
 app.get("/", (req, res) => {
-    res.send("Backend is running for JD assignment");
+    res.send("Backend is running");
 });
 
 
